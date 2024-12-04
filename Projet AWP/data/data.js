17863a1a4d4ca35e2ba8abc0202ab02e464cb369
LowAlex <55943432+LowAlex@users.noreply.github.com>
2024-12-04 12:23:03 -0400

    Update data.js

--- Projet AWP/data/data.js
+++ Projet AWP/data/data.js
@@ -2,22 +2,22 @@ export const items = [
     {
       title: "Mercury",
       desc: "Mercury is the smallest planet in the Solar System and the closest to the Sun. It has no atmosphere and experiences extreme temperature fluctuations.",
-      img: "../img/mercury.jpg"
+      img: "./img/mercury.jpg"
     },
     {
       title: "Venus",
       desc: "Venus is the second planet from the Sun and is often called Earth's twin due to its similar size. Its thick atmosphere traps heat, making it the hottest planet.",
-      img: "../img/venus.jpeg"
+      img: "./img/venus.jpeg"
     },
     {
       title: "Earth",
       desc: "Earth is the third planet from the Sun and the only known planet to support life. It has a balanced atmosphere and liquid water covering 71% of its surface.",
-      img: "../img/earth.jpeg"
+      img: "./img/earth.jpeg"
     },
     {
       title: "Mars",
       desc: "Mars, the fourth planet from the Sun, is known as the Red Planet due to its iron-rich surface. It has the largest volcano in the Solar System, Olympus Mons.",
-      img: "../img/mars.jpeg"
+      img: "./img/mars.jpeg"
     },
     {
       title: "Jupiter",
