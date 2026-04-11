f1dea858a374a97cb4fe14bccf7d4b386b602672
LowAlex <lebloisalexan@gmail.com>
2026-04-11 12:44:11 +0200

    ajout planet model et migration

--- planets/models.py
+++ planets/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Planet(models.Model):
+    title = models.CharField(max_length=100)
+    desc = models.TextField()
+    img = models.ImageField(upload_to='planets/')
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return self.title
