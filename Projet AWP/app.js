9e812c9c0ff2aaf49a720d6a25e0cc4e6501bf53
LowAlex <lebloisalexan@gmail.com>
2026-04-12 16:22:49 +0200

    Connect Vue.js frontend to Django API

--- Projet AWP/app.js
+++ Projet AWP/app.js
@@ -1,12 +1,19 @@
-import { items } from './data/data.js';
-
 const app = Vue.createApp({
   data() {
     return {
-      items: items,
+      items: [],
       currentIndex: 0,
       showTitle: true
     };
+  },
+  async mounted() {
+    const response = await fetch('http://127.0.0.1:8000/api/planets/');
+    const data = await response.json();
+    this.items = data.map(p => ({
+      title: p.title,
+      desc: p.desc,
+      img: 'http://127.0.0.1:8000/media/' + p.img
+    }));
   },
   computed: {
     currentItem() {
